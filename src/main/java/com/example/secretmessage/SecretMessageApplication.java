0420aafd5d1e89e795a0de8d926c0612d6434b62
Teddy <kosytedy2@gmail.com>
2020-10-08 19:27:48 +0100

    first commit

---
+++ src/main/java/com/example/secretmessage/SecretMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.secretmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretMessageApplication.class, args);
+	}
+
+}
